2e69801c17a4c2138accba57942b2ea65d53f689
Doru Laslau <doru.laslau@gmail.com>
2025-03-21 13:57:03 +0000

    file for new view in snowflake

--- models/example/dimcustomers.sql
+++ models/example/dimcustomers.sql
@@ -1,8 +1,3 @@
-{{
-    config(materialized='table')
-}}
-
-
 use warehouse COMPUTE_WH;
 
-select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER where c_acctbal > 9000 order by c_acctbal desc
+select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER where c_acctbal > 9900 order by c_acctbal desc
