2a8ff893244fc40a66215ffcfa92903295cbc45c
Doru Laslau <doru.laslau@gmail.com>
2025-03-21 13:50:19 +0000

    let`s see changes in snowflake

--- models/example/dimcustomers.sql
+++ models/example/dimcustomers.sql
@@ -1,1 +1,8 @@
-select 1 as doru
+{{
+    config(materialized='table')
+}}
+
+
+use warehouse COMPUTE_WH;
+
+select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER where c_acctbal > 9000 order by c_acctbal desc
